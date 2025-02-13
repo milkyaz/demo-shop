fbed6de2f45912867c0b087cee8380ac95fd970e
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-02-13 23:31:18 +0300

    разбил на компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-
+import Box from "@mui/material/Box";
+import "./App.css";
 import GoodsToDisplay from "./components/GoodsToDisplay/GoodsToDisplay";
 import Filter from "./components/Filter/Filter";
 import Search from "./components/Search/Search";
 import Sidebar from "./components/Sidebar/Sidebar";
 import UseGeoLocation from "./components/UseGeoLocation/UseGeoLocation";
+import Container from "@mui/material/Container";
 
 const IMAGES = {
   image1: new URL("./img/React.png", import.meta.url).href,
@@ -52,43 +54,60 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <>
-        <header
-          className="header"
-          style={{ display: "flex", alignItems: "center", width: "983px" }}
-        >
-          <img
-            src={IMAGES.image1}
-            alt="react"
-            className="header__logo"
-            style={{ width: "103px", height: "43px" }}
-          />
-          <UseGeoLocation />
-          <Search
-            searchTerm={searchItem}
-            setSearchTerm={setSearchItem}
-            onChange={handleSearchItem}
-          />
-        </header>
+    <Container fixed>
+      <div className="App">
+        <div className="main-shop">
+          <header
+            className="header"
+            style={{ display: "flex", alignItems: "center", width: "983px" }}
+          >
+            <img
+              src={IMAGES.image1}
+              alt="react"
+              className="header__logo"
+              style={{ width: "103px", height: "43px" }}
+            />
+            <UseGeoLocation />
+            <Search
+              searchTerm={searchItem}
+              setSearchTerm={setSearchItem}
+              onChange={handleSearchItem}
+            />
+          </header>
 
-        <Filter
-          category={selectCategory}
-          handleCategoryChange={handleCategoryChange}
-          goods={goods}
-        />
-        {filteredCharactersToDisplay().map((goods) => (
-          <GoodsToDisplay
-            title={goods.title}
-            price={goods.price}
-            category={goods.category}
-            image={goods.image}
-            key={goods.id}
+          <Filter
+            category={selectCategory}
+            handleCategoryChange={handleCategoryChange}
+            goods={goods}
           />
-        ))}
+          <Box
+            className="box-shop"
+            sx={{
+              display: "flex",
+              gap: "40px",
+              flexWrap: "wrap",
+              marginTop: "50px",
+              width: "916px",
+              height: "500px",
+              border: "1px solid",
+              justifyContent: "center",
+              overflow: "auto",
+            }}
+          >
+            {filteredCharactersToDisplay().map((goods) => (
+              <GoodsToDisplay
+                title={goods.title}
+                price={goods.price}
+                category={goods.category}
+                image={goods.image}
+                key={goods.id}
+              />
+            ))}
+          </Box>
+        </div>
         <Sidebar />
-      </>
-    </div>
+      </div>
+    </Container>
   );
 }
 
--- src/components/GoodsToDisplay/GoodsToDisplay.jsx
+++ src/components/GoodsToDisplay/GoodsToDisplay.jsx
@@ -3,20 +3,7 @@ import Box from "@mui/material/Box";
 import "./GoodsToDisplay";
 function GoodsToDisplay({ characters, title, price, category, image }) {
   return (
-    <Box
-      className="box-shop"
-      sx={{
-        display: "flex",
-        gap: "40px",
-        flexWrap: "wrap",
-        marginTop: "50px",
-        width: "916px",
-        height: "500px",
-        border: "1px solid",
-        justifyContent: "center",
-        overflow: "auto",
-      }}
-    >
+    <Box className="box-shop">
       <GoodsItem
         title={title}
         price={price}
